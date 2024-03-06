f8ac8d6a443bc59755c7dc7d6d0fc1cb5dd85ef3
Wajahat Mansoori <wajahatmansoori11@gmail.com>
2024-03-06 04:26:40 -0800

    User detail post APi

--- PortalWebAPIs/Controllers/UserController.cs
+++ PortalWebAPIs/Controllers/UserController.cs
@@ -9,19 +9,21 @@ using System.Web.Http;
 using System.Configuration;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-
+using PortalWebAPIs.Models;
 
 namespace PortalWebAPIs.Controllers
 {
     public class UserController : ApiController
     {
+        string connectionString = ConfigurationManager.ConnectionStrings["HrsConn"].ToString();
+
         [HttpGet]
         [Route("api/GetUserInfo")]
         public IHttpActionResult GetUserDetails()
         {
             List<Dictionary<string, object>> userDetailsList = new List<Dictionary<string, object>>();
 
-            string connectionString = ConfigurationManager.ConnectionStrings["HrsConn"].ToString();
+            
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
@@ -36,12 +38,11 @@ namespace PortalWebAPIs.Controllers
                         while (reader.Read())
                         {
                             var userDetails = new Dictionary<string, object>
-                    {
-                        { "UserId", reader.GetInt32(0) },
-                        { "FullName", reader.GetString(1) },
-                        { "DepartmentName", reader.GetString(2) }
-                    };
-
+                             {
+                                { "UserId", reader.GetInt32(0) },
+                                { "FullName", reader.GetString(1) },
+                                { "DepartmentName", reader.GetString(2) }
+                            };
                             userDetailsList.Add(userDetails);
                         }
                     }
@@ -50,6 +51,65 @@ namespace PortalWebAPIs.Controllers
 
             return Json(userDetailsList);
         }
+
+        [HttpPost]
+        [Route("api/AddUserInfo")]
+        public IHttpActionResult AddUserDetail(UserDetail model)
+        {
+            try
+            {
+                List<Dictionary<string, object>> userDetailsList = new List<Dictionary<string, object>>();
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand("API_InsertUserDetails", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@UserLoginId", model.UserLoginId);
+                        cmd.Parameters.AddWithValue("@FullName", model.FullName);
+                        cmd.Parameters.AddWithValue("@DeptId", model.DeptId);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var userDetails = new Dictionary<string, object>
+                             {
+                                { "Sno", reader.GetInt32(0) },
+                                { "UserId", reader.GetInt32(1) },
+                                { "FullName", reader.GetString(2) },
+                                { "DepartmentName", reader.GetString(3) }
+                            };
+                                userDetailsList.Add(userDetails);
+                            }
+                        }
+                        //cmd.Parameters.AddWithValue("@UserLoginId", model.UserLoginId);
+                        //cmd.Parameters.AddWithValue("@FullName", model.FullName);
+                        //cmd.Parameters.AddWithValue("@DeptId", model.DeptId);
+
+                        //con.Open();
+                        //int Rows = cmd.ExecuteNonQuery();
+                        //if (Rows > 0)
+                        //{
+                        //    return true;
+                        //}
+                        //else
+                        //{
+                        //    return false;
+                        //}
+                    }
+                    con.Close();
+                }
+                return Json(userDetailsList);
+            }
+            catch (Exception ex)
+            {
+                // Log or handle the exception as needed
+                throw ex;
+            }
+        }
+
 
 
 
